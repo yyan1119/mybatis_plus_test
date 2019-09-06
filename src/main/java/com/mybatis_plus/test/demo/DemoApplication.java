ac4125ef55fe25849350da9e4d3921c530d81d79
yyan1119 <yyan1119@126.com>
2019-09-06 17:23:47 +0800

    Initial commit

---
+++ src/main/java/com/mybatis_plus/test/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.mybatis_plus.test.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
